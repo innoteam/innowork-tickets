19cb038e96efe338990ba18bf7b50702e12e68b0
Alex Pagnoni <alex.pagnoni@innoteam.it>
2014-07-23 14:18:18 +0200

    Fixed missing labels in items search results. Fixed missing relations in assignedto and openedby fields.

--- source/core/classes/innowork/tickets/InnoworkTicket.php
+++ source/core/classes/innowork/tickets/InnoworkTicket.php
@@ -34,8 +34,8 @@ class InnoworkTicket extends InnoworkItem
         $this->mKeys['typeid'] = 'table:innowork_tickets_fields_values:fieldvalue:integer';
         $this->mKeys['creationdate'] = 'timestamp';
         $this->mKeys['done'] = 'boolean';
-        $this->mKeys['openedby'] = 'integer';
-        $this->mKeys['assignedto'] = 'integer';
+        $this->mKeys['openedby'] = 'table:domain_users:username:integer';
+        $this->mKeys['assignedto'] = 'table:domain_users:username:integer';
 
         $this->mSearchResultKeys[] = 'title';
         $this->mSearchResultKeys[] = 'projectid';
